05a650132aa73a71a07e025f219752f7a5c9b82c
Gareth G. Waughan <44147527+Gardyion@users.noreply.github.com>
2023-12-12 04:51:45 +0000

    Bash Functions

---
+++ functions.sh
@@ -0,0 +1,14 @@
+#######################################
+# Description: Hello function
+# Globals:
+#   None
+# Arguments:
+#   Single input argument
+# Outputs:
+#   Value of input argument
+# Returns:
+#   0 if successful, non-zero on error.
+#######################################
+function hello() {
+    echo "Hello $1!"
+}
